a80baa68178d4adb7b27841aad2b987cacfb4f97
Deejay0028 <163960989+Deejay0028@users.noreply.github.com>
2024-03-19 11:55:14 +0800

    Update module-info.java

--- module-info.java
+++ module-info.java
@@ -1,9 +1,5 @@
 module application {
     requires javafx.controls;
     requires javafx.fxml;
-
-    //if necessary, please activate:
-    //exports application;
-    //requires transitive javafx.graphics;
    
 }
