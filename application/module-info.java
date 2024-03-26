a47b4319f9e5e3cc33bcf6b0932bec14fcfb613c
rox <roxmarzan14@gmail.com>
2024-03-26 08:55:14 +0800

    updates

---
+++ application/module-info.java
@@ -0,0 +1,9 @@
+module application {
+    requires javafx.controls;
+    requires javafx.fxml;
+	requires javafx.graphics;
+
+    //if necessary, please activate:
+    //exports application;
+    //requires transitive javafx.graphics;
+}
